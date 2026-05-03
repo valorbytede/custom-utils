4688735873ad08a2df422727d1c511f9ac5147e5
hurriCase <dmitriy.pastanovskiy@gmail.com>
2026-05-03 11:50:25 +0300

    feat: Unit enum for single state animation

---
+++ Runtime/Animations/Base/Unit.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+
+namespace CustomUtils.Runtime.Animations.Base
+{
+    /// <summary>
+    /// A single-value enum used as a state type for animations that don't require multiple states.
+    /// </summary>
+    [PublicAPI]
+    public enum Unit
+    {
+        Default = 0
+    }
+}
